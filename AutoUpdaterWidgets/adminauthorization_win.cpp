b1e6222f779b55ffdd7c76ab3e44d91b26d8df91
Skycoder42 <Skycoder42@users.noreply.github.com>
2015-12-25 21:50:44 +0100

    linux root -> ?!?!?!

--- AutoUpdaterWidgets/adminauthorization_win.cpp
+++ AutoUpdaterWidgets/adminauthorization_win.cpp
@@ -38,9 +38,6 @@
 #include <qt_windows.h>
 using namespace QtAutoUpdater;
 
-// taken from qprocess_win.cpp
-static QString qt_create_commandline(const QString &program, const QStringList &arguments);
-
 struct DeCoInitializer
 {
     DeCoInitializer()
@@ -107,43 +104,4 @@ bool AdminAuthorization::executeAsAdmin(const QString &program, const QStringLis
         return true;
 	else
 		return false;
-}
-
-static QString qt_create_commandline(const QString &program, const QStringList &arguments)
-{
-	QString args;
-	if (!program.isEmpty()) {
-		QString programName = program;
-		if (!programName.startsWith(QLatin1Char('\"')) && !programName.endsWith(QLatin1Char('\"'))
-			&& programName.contains(QLatin1Char(' '))) {
-				programName = QLatin1Char('\"') + programName + QLatin1Char('\"');
-		}
-		programName.replace(QLatin1Char('/'), QLatin1Char('\\'));
-
-		// add the program as the first arg ... it works better
-		args = programName + QLatin1Char(' ');
-	}
-
-	for (int i = 0; i < arguments.size(); ++i) {
-		QString tmp = arguments.at(i);
-		// in the case of \" already being in the string the \ must also be escaped
-		tmp.replace(QLatin1String("\\\""), QLatin1String("\\\\\""));
-		// escape a single " because the arguments will be parsed
-		tmp.replace(QLatin1Char('\"'), QLatin1String("\\\""));
-		if (tmp.isEmpty() || tmp.contains(QLatin1Char(' ')) || tmp.contains(QLatin1Char('\t'))) {
-			// The argument must not end with a \ since this would be interpreted
-			// as escaping the quote -- rather put the \ behind the quote: e.g.
-			// rather use "foo"\ than "foo\"
-			QString endQuote(QLatin1Char('\"'));
-			int i = tmp.length();
-			while (i > 0 && tmp.at(i - 1) == QLatin1Char('\\')) {
-				--i;
-				endQuote += QLatin1Char('\\');
-			}
-			args += QLatin1String(" \"") + tmp.left(i) + endQuote;
-		} else {
-			args += QLatin1Char(' ') + tmp;
-		}
-	}
-	return args;
 }
--- AutoUpdaterWidgets/adminauthorization_x11.cpp
+++ AutoUpdaterWidgets/adminauthorization_x11.cpp
@@ -1,42 +1,12 @@
-/**************************************************************************
-**
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
-**
-** This file is part of the Qt Installer Framework.
-**
-** $QT_BEGIN_LICENSE:LGPL$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see http://qt.io/terms-conditions. For further
-** information use the contact form at http://www.qt.io/contact-us.
-**
-** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 or version 3 as published by the Free
-** Software Foundation and appearing in the file LICENSE.LGPLv21 and
-** LICENSE.LGPLv3 included in the packaging of this file. Please review the
-** following information to ensure the GNU Lesser General Public License
-** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** As a special exception, The Qt Company gives you certain additional
-** rights. These rights are described in The Qt Company LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-**
-** $QT_END_LICENSE$
-**
-**************************************************************************/
-
 #include "adminauthorization.h"
 #include <unistd.h>
 #include <sys/types.h>
 #include <QCoreApplication>
-#include "messagemaster.h"
+#include <QProcess>
+#include <QTemporaryFile>
+#include <QTextStream>
+#include <QInputDialog>
+#include <QThread>
 using namespace QtAutoUpdater;
 
 // has no guarantee to work
@@ -45,11 +15,38 @@ bool AdminAuthorization::hasAdminRights()
     return getuid() == 0;
 }
 
-bool AdminAuthorization::executeAsAdmin(const QString &, const QStringList &, const QString &)//TODO wokring dir...
+bool AdminAuthorization::executeAsAdmin(const QString &program, const QStringList &arguments, const QString &workingDirectory)
 {
-    MessageMaster::warning(NULL,
-                           QCoreApplication::translate("AdminAuthorization", "Error"),
-                           QCoreApplication::translate("AdminAuthorization", "Running as root is not supported for Linux systems right now!"));
-    return false;
+    bool ok = false;
+    QString password = QInputDialog::getText(NULL,
+                                             QCoreApplication::translate("AdminAuthorization", "Run as root"),
+                                             QCoreApplication::translate("AdminAuthorization", "Please enter the root password to run the maintenancetool as root:"),
+                                             QLineEdit::Password,
+                                             QString(),
+                                             &ok,
+                                             Qt::WindowCloseButtonHint | Qt::WindowStaysOnTopHint);
+    if(!ok)
+        return false;
+
+
+    QTemporaryFile tFile;
+    ((QIODevice*)&tFile)->open(QIODevice::WriteOnly | QIODevice::Text);
+
+    QStringList args;
+    args += program;
+    args += arguments;
+    QTextStream tStream(&tFile);
+    tStream << "#!/bin/bash\n"
+            << "cd " << workingDirectory << "\n"
+            << "echo " << password << " | sudo -S" << qt_create_commandline(args) << " &\n";
+    tStream.flush();
+
+    tFile.close();
+    tFile.setPermissions(tFile.permissions() | QFileDevice::ExeOther);
+
+    if(QProcess::execute("/bin/bash", {tFile.fileName()}) == 0) {
+        return true;
+    } else
+        return false;
 }
 
