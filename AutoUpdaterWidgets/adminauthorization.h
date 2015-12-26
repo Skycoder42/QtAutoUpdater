65e3fd3b16ac5ab5e5e1eb32df08a22e56c83816
Skycoder42 <Skycoder42@users.noreply.github.com>
2015-12-26 13:08:30 +0100

    .

--- AutoUpdaterWidgets/adminauthorization.h
+++ AutoUpdaterWidgets/adminauthorization.h
@@ -9,38 +9,8 @@ namespace QtAutoUpdater
 	{
 	public:
 		bool hasAdminRights() Q_DECL_OVERRIDE;
-		bool executeAsAdmin(const QString &program, const QStringList &arguments, const QString &workingDir) Q_DECL_OVERRIDE;
-
-    private:
-        static inline QString qt_create_commandline(const QStringList &arguments);
+        bool executeAsAdmin(const QString &program, const QStringList &arguments, const QString &workingDir) Q_DECL_OVERRIDE;
     };
-
-    QString AdminAuthorization::qt_create_commandline(const QStringList &arguments)
-    {
-        QString args;
-        for (int i = 0; i < arguments.size(); ++i) {
-            QString tmp = arguments.at(i);
-            // in the case of \" already being in the string the \ must also be escaped
-            tmp.replace(QLatin1String("\\\""), QLatin1String("\\\\\""));
-            // escape a single " because the arguments will be parsed
-            tmp.replace(QLatin1Char('\"'), QLatin1String("\\\""));
-            if (tmp.isEmpty() || tmp.contains(QLatin1Char(' ')) || tmp.contains(QLatin1Char('\t'))) {
-                // The argument must not end with a \ since this would be interpreted
-                // as escaping the quote -- rather put the \ behind the quote: e.g.
-                // rather use "foo"\ than "foo\"
-                QString endQuote(QLatin1Char('\"'));
-                int i = tmp.length();
-                while (i > 0 && tmp.at(i - 1) == QLatin1Char('\\')) {
-                    --i;
-                    endQuote += QLatin1Char('\\');
-                }
-                args += QLatin1String(" \"") + tmp.left(i) + endQuote;
-            } else {
-                args += QLatin1Char(' ') + tmp;
-            }
-        }
-        return args;
-    }
 }
 
 #endif // ADMINAUTHORIZATION_H
--- AutoUpdaterWidgets/adminauthorization_win.cpp
+++ AutoUpdaterWidgets/adminauthorization_win.cpp
@@ -38,6 +38,8 @@
 #include <qt_windows.h>
 using namespace QtAutoUpdater;
 
+static QString AdminAuthorization::qt_create_commandline(const QStringList &arguments);
+
 struct DeCoInitializer
 {
     DeCoInitializer()
@@ -104,4 +106,31 @@ bool AdminAuthorization::executeAsAdmin(const QString &program, const QStringLis
         return true;
 	else
 		return false;
+}
+
+QString AdminAuthorization::qt_create_commandline(const QStringList &arguments)
+{
+    QString args;
+    for (int i = 0; i < arguments.size(); ++i) {
+        QString tmp = arguments.at(i);
+        // in the case of \" already being in the string the \ must also be escaped
+        tmp.replace(QLatin1String("\\\""), QLatin1String("\\\\\""));
+        // escape a single " because the arguments will be parsed
+        tmp.replace(QLatin1Char('\"'), QLatin1String("\\\""));
+        if (tmp.isEmpty() || tmp.contains(QLatin1Char(' ')) || tmp.contains(QLatin1Char('\t'))) {
+            // The argument must not end with a \ since this would be interpreted
+            // as escaping the quote -- rather put the \ behind the quote: e.g.
+            // rather use "foo"\ than "foo\"
+            QString endQuote(QLatin1Char('\"'));
+            int i = tmp.length();
+            while (i > 0 && tmp.at(i - 1) == QLatin1Char('\\')) {
+                --i;
+                endQuote += QLatin1Char('\\');
+            }
+            args += QLatin1String(" \"") + tmp.left(i) + endQuote;
+        } else {
+            args += QLatin1Char(' ') + tmp;
+        }
+    }
+    return args;
 }
