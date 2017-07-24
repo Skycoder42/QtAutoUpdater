2e921d21003e8f979a1a605277fc97cb569ba92f
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-24 12:59:55 +0200

    update repogen

--- repogen.sh
+++ repogen.sh
@@ -4,4 +4,7 @@
 # $2 Version
 
 myDir=$(dirname "$0")
-"$myDir/../QtModules/repogen.py" "$1" AutoUpdater "" "" "A Qt library to automatically check for updates and install them." "$2" "$myDir/LICENSE" BSD-3-Clause "android_armv7,android_x86,ios,winrt_x86_msvc2017,winrt_x64_msvc2017"
+qtDir=${1?First parameter must be set to the dir to install}
+version=${2?Set the version as second parameter}
+
+"$myDir/../QtModules/deploy/repogen.py" "$qtDir" AutoUpdater "" "" "A Qt library to automatically check for updates and install them." "$version" "$myDir/LICENSE" BSD-3-Clause  "android_armv7,android_x86,ios,winrt_x86_msvc2017,winrt_x64_msvc2017,winrt_armv7_msvc2017"
