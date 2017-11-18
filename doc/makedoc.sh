669db38cb4320c10f59cab96f3ecd2d4495b4a4f
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-11-18 18:27:22 +0100

    small stuff

--- doc/makedoc.sh
+++ doc/makedoc.sh
@@ -16,34 +16,35 @@ qtBins=$3
 qtHeaders=$4
 qtDocs=$5
 doxyTemplate="$srcDir/Doxyfile"
+doxyRes=Doxyfile.generated
 readme="$destDir/README.md"
 doxme="$scriptDir/doxme.py"
 
 python3 "$doxme" "$srcDir/../README.md"
 
-cat "$doxyTemplate" > Doxyfile
-echo "PROJECT_NUMBER = \"$version\"" >> Doxyfile
-echo "INPUT += \"$readme\"" >> Doxyfile
-echo "USE_MDFILE_AS_MAINPAGE = \"$readme\"" >> Doxyfile
-echo "OUTPUT_DIRECTORY = \"$destDir\"" >> Doxyfile
-echo "QHP_NAMESPACE = \"de.skycoder42.qtautoupdater.$verTag\"" >> Doxyfile
-echo "QHP_CUST_FILTER_NAME = \"AutoUpdater $version\"" >> Doxyfile
-echo "QHP_CUST_FILTER_ATTRS = \"qtautoupdater $version\"" >> Doxyfile
-echo "QHG_LOCATION = \"$qtBins/qhelpgenerator\"" >> Doxyfile
-echo "INCLUDE_PATH += \"$qtHeaders\"" >> Doxyfile
-echo "GENERATE_TAGFILE = \"$destDir/qtautoupdater/qtautoupdater.tags\"" >> Doxyfile
+cat "$doxyTemplate" > $doxyRes
+echo "PROJECT_NUMBER = \"$version\"" >> $doxyRes
+echo "INPUT += \"$readme\"" >> $doxyRes
+echo "USE_MDFILE_AS_MAINPAGE = \"$readme\"" >> $doxyRes
+echo "OUTPUT_DIRECTORY = \"$destDir\"" >> $doxyRes
+echo "QHP_NAMESPACE = \"de.skycoder42.qtautoupdater.$verTag\"" >> $doxyRes
+echo "QHP_CUST_FILTER_NAME = \"AutoUpdater $version\"" >> $doxyRes
+echo "QHP_CUST_FILTER_ATTRS = \"qtautoupdater $version\"" >> $doxyRes
+echo "QHG_LOCATION = \"$qtBins/qhelpgenerator\"" >> $doxyRes
+echo "INCLUDE_PATH += \"$qtHeaders\"" >> $doxyRes
+echo "GENERATE_TAGFILE = \"$destDir/qtautoupdater/qtautoupdater.tags\"" >> $doxyRes
 if [ "$DOXY_STYLE" ]; then
-	echo "HTML_STYLESHEET = \"$DOXY_STYLE\"" >> Doxyfile
+	echo "HTML_STYLESHEET = \"$DOXY_STYLE\"" >> $doxyRes
 fi
 if [ "$DOXY_STYLE_EXTRA" ]; then
-	echo "HTML_EXTRA_STYLESHEET = \"$DOXY_STYLE_EXTRA\"" >> Doxyfile
+	echo "HTML_EXTRA_STYLESHEET = \"$DOXY_STYLE_EXTRA\"" >> $doxyRes
 fi
 
 for tagFile in $(find "$qtDocs" -name *.tags); do
 	if [ $(basename "$tagFile") != "qtautoupdater.tags" ]; then
-		echo "TAGFILES += "$tagFile=https://doc.qt.io/qt-5"" >> Doxyfile
+		echo "TAGFILES += \"$tagFile=https://doc.qt.io/qt-5\"" >> $doxyRes
 	fi
 done
 
 cd "$srcDir"
-doxygen "$destDir/Doxyfile"
+doxygen "$destDir/$doxyRes"
