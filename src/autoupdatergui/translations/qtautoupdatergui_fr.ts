8d96a7402eb00c5067cbe2fddca5bbf66d9ecc57
Alex <aalex420@users.noreply.github.com>
2018-01-22 23:31:14 +0100

    Add files via upload

---
+++ src/autoupdatergui/translations/qtautoupdatergui_fr.ts
@@ -0,0 +1,266 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>AdminAuthorization</name>
+    <message>
+        <location filename="../adminauthorization_x11.cpp" line="+193"/>
+        <source>Enter Password</source>
+        <translation>Entrez votre mot de passe</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Enter your root password to run the maintenancetool:</source>
+        <translation>Entrez votre mot de passe pour lancer le logiciel de maintenance:</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressDialog</name>
+    <message>
+        <location filename="../progressdialog.ui" line="+17"/>
+        <source>Check for Updates</source>
+        <translation>Cherche les mises à jour</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Checking for updates…</source>
+        <translation>Vérifiaction des mises à jour...</translation>
+    </message>
+</context>
+<context>
+    <name>QtAutoUpdater::ProgressDialog</name>
+    <message>
+        <location filename="../progressdialog.cpp" line="+36"/>
+        <source>Canceling update check…</source>
+        <translation>Annuler la vérification des mises à jour...</translation>
+    </message>
+</context>
+<context>
+    <name>QtAutoUpdater::UpdateButton</name>
+    <message>
+        <location filename="../updatebutton.cpp" line="+95"/>
+        <source>Checking for updates…</source>
+        <translation>Vérification des mises à jour...</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>New Update!</source>
+        <translation>Nouvelle mise à jour!</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>No new updates available</source>
+        <translation>Aucune mise à jour disponible</translation>
+    </message>
+</context>
+<context>
+    <name>QtAutoUpdater::UpdateController</name>
+    <message>
+        <location filename="../updatecontroller.cpp" line="+40"/>
+        <location line="+98"/>
+        <location line="+12"/>
+        <location line="+75"/>
+        <location line="+53"/>
+        <location line="+4"/>
+        <source>Check for Updates</source>
+        <translation>Vérification des mises à jour</translation>
+    </message>
+    <message>
+        <location line="-143"/>
+        <source>Do you want to check for updates now?</source>
+        <translation>Voulez-vous rechercher des mises à jour maintenant ?</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>The program is already checking for updates!</source>
+        <translation>Le programme cherche déjà des mises à jour !</translation>
+    </message>
+    <message>
+        <location line="+75"/>
+        <source>Checking for updates was canceled!</source>
+        <translation>La recherche de mises à jour à été annulée !</translation>
+    </message>
+    <message>
+        <location line="+35"/>
+        <source>Install Updates</source>
+        <translation>Installer les mises à jour</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>New updates are available. The maintenance tool will be started to install those as soon as you close the application!</source>
+        <translation>De nouvelles mises à jour sont disponibles. Le logiciel de maintenance va être lancé pour installer les mises à jour quand l&apos;application sera fermée!</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>The update process crashed!</source>
+        <translation>Le processus de mises à jour a crashé!</translation>
+    </message>
+    <message>
+        <location line="-4"/>
+        <source>No new updates available!</source>
+        <translation>Aucune mises à jour disponibles!</translation>
+    </message>
+    <message>
+        <location line="-236"/>
+        <source>Checks if new updates are available. You will be prompted before updates are installed.</source>
+        <translation>Vérifier si des mises à jour sont disponibles. Vous serez prevenu avant que les mises à jour s&apos;installent.</translation>
+    </message>
+</context>
+<context>
+    <name>QtAutoUpdater::UpdateInfoDialog</name>
+    <message>
+        <location filename="../updateinfodialog.cpp" line="+33"/>
+        <location line="+14"/>
+        <source>Updates for %1 are available!</source>
+        <translation>Mise à jour pour %1 disponible(s) !</translation>
+    </message>
+    <message>
+        <location line="+40"/>
+        <location line="+48"/>
+        <source>%L1 Bytes</source>
+        <translation>%L1 Octets</translation>
+    </message>
+    <message>
+        <location line="-73"/>
+        <source>Install Now</source>
+        <translation>Installer maintenant</translation>
+    </message>
+    <message>
+        <location line="+45"/>
+        <source>Close the application and install updates?</source>
+        <translation>Fermer l&apos;application et installer les mises à jour ?</translation>
+    </message>
+    <message>
+        <location line="-44"/>
+        <location line="+53"/>
+        <source>Install On Exit</source>
+        <translation>Installer à la fermeture</translation>
+    </message>
+    <message>
+        <location line="-71"/>
+        <source>Check for Updates</source>
+        <translation>Vérifier les mises à jour</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>There are new updates available! You can install them now or later.</source>
+        <translation>Il y a des mises à jour disponibles! Vous pouvez les installer maintenant ou plutard.</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>%1 v%2 — %3</source>
+        <translation>%1 v%2 — %3</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Run with &amp;elevated rights</source>
+        <translation>Lancer avec les droits d&apos;&amp;aministrateurs</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Install later</source>
+        <translation>Istaller plutard</translation>
+    </message>
+    <message>
+        <location line="+42"/>
+        <source>Install Now?</source>
+        <translation>Installer maintenant ?</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Updates will be installed on exit. The maintenance tool will be started as soon as you close the application!</source>
+        <translation>Les mises à jour vont êtres installées à la fermeture de l&apos;application. Le logiciel de maintenance va être lancé dès que vous aurez quitté l&apos;application !</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>%L1 KiB</source>
+        <translation>%L1 KiO</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>%L1 MiB</source>
+        <translation>%L1 MiO</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>%L1 GiB</source>
+        <translation>%L1 GiO</translation>
+    </message>
+</context>
+<context>
+    <name>UpdateButton</name>
+    <message>
+        <location filename="../updatebutton.ui" line="+35"/>
+        <source>Check for Updates</source>
+        <translation>Vérifier les mises à jour</translation>
+    </message>
+</context>
+<context>
+    <name>UpdateInfoDialog</name>
+    <message>
+        <location filename="../updateinfodialog.ui" line="+17"/>
+        <source>Check for Updates</source>
+        <translation>Vérifier les mises à jour</translation>
+    </message>
+    <message>
+        <location line="+35"/>
+        <source>There are new updates available! You can install them now or automatically, when you exit the application. If you don&apos;t want to install updates right know, you can install them later.</source>
+        <translation>Il y a des mises à jour disponibles! Vous pouvez les installer maintenant ou automatiquement, quand vous aurrez quitté l&apos;application. Si vous ne voulez pas installer les mises à jour maintenant, vous pouvez les installer plutard.</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Updates:</source>
+        <translation>Mises à jour :</translation>
+    </message>
+    <message>
+        <location line="+34"/>
+        <source>Component Name</source>
+        <translation>Nom du composant</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Update Size</source>
+        <translation>Date de mise à jour</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>Run with &amp;elevated rights</source>
+        <translation>Lancer avec les droits d&apos;&amp;administrateur</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Close and do nothing. You can install updates later.</source>
+        <translation>Fermer et ne rien faire. Vous pouvez installer les mises à jour plutard.</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Install later</source>
+        <translation>Installer plutard</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>You proceed using the application, and as soon as you close it, the updater will start automatically to install the updates.</source>
+        <translation>Vous continuez à utiliser l&apos;application et, dès que vous la fermez, la mise à jour démarrera automatiquement pour installer les mises à jour.</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Install On Exit</source>
+        <translation>Installer à la fermeture</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Closes the application and opens the installer to update the app.</source>
+        <translation>Fermer l&apos;application et ouvrir l&apos;installateur pour mettre à jour l&apos;application.</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Install Now</source>
+        <translation>Installer maintenant</translation>
+    </message>
+</context>
+</TS>
