8dc595046af75b5ac0bbd1ef8a644f6ad04f11a8
Alex <aalex420@users.noreply.github.com>
2019-02-19 01:47:00 +0100

    Update main.cpp

--- examples/autoupdatergui/simpleUpdaterGui/main.cpp
+++ examples/autoupdatergui/simpleUpdaterGui/main.cpp
@@ -9,22 +9,19 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 	
     //Translate app
-    QString locale = QLocale::system().name().section('_', 0, 0);
     QTranslator translator;
-    translator.load(QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    translator.load(QLocale(), QStringLiteral("qt"), QStringLiteral("_"), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
 
     a.installTranslator(&translator);
 
     //Translate updater
     QTranslator translatorUpdater;
-
-    translatorUpdater.load(QString("translations/qtautoupdatergui_") + locale + QString(".qm"));
+    translatorUpdater.load(QLocale(), QStringLiteral("qtautoupdatergui"), QStringLiteral("_"), QStringLiteral("translations"));
 
     a.installTranslator(&translatorUpdater);
 
     //Launch GUI
     MainWindow w;
-
     w.show();
 
     return a.exec();
