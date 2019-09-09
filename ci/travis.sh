93f4b22bd580a30851f4818167619dd441dd14c2
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-09-09 22:17:09 +0200

    ..

--- ci/travis.sh
+++ ci/travis.sh
@@ -7,7 +7,8 @@ if [ $PLATFORM == "gcc_64" ]; then
 	scriptDir=../src/3rdparty/PackageKit-Qt
 	sed -i 's/set(BUILD_SHARED_LIBS ON)/set(BUILD_SHARED_LIBS OFF)/g' "$scriptDir/CMakeLists.txt"
 
-	PATH=/opt/qt/$QT_VER/$PLATFORM/bin:$PATH cmake $scriptDir
+	export CMAKE_PREFIX_PATH=/opt/qt/$QT_VER/$PLATFORM:$CMAKE_PREFIX_PATH
+	cmake $scriptDir
 	make
 	make install
 	
