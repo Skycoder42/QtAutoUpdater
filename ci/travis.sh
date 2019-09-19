5d722def2eb82327c8a35f94ebd2e3283fcf1031
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-09-19 07:52:12 +0200

    .

--- ci/travis.sh
+++ ci/travis.sh
@@ -7,7 +7,7 @@ currDir=$(dirname $0)
 if [[ $PLATFORM == "gcc_64" ]]; then
 	# prepend pre build script
 	mv qtmodules-travis/ci/linux/build-docker.sh qtmodules-travis/ci/linux/build-docker.sh.orig
-	cat "$currDir/travis_pkgkit.sh" > qtmodules-travis/ci/linux/build-docker.sh
+	mv "$currDir/travis_pkgkit.sh" qtmodules-travis/ci/linux/build-docker.sh
 	cat "$currDir/travis_qthttpserver.sh" >> qtmodules-travis/ci/linux/build-docker.sh
 	cat qtmodules-travis/ci/linux/build-docker.sh.orig >> qtmodules-travis/ci/linux/build-docker.sh
 elif [[ $PLATFORM == "clang_64" ]]; then
